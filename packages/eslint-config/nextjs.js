5acfc69e03b94046b2458b2400ab4840112c33cd
raiton-boo <raito.build@gmail.com>
2026-08-20 17:36:36 +0900

    fix: eslint-config-nextの読み込みをFlatCompatからネイティブFlat Configに修正

--- packages/eslint-config/nextjs.js
+++ packages/eslint-config/nextjs.js
@@ -1,16 +1,5 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import coreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypescript from 'eslint-config-next/typescript';
 import base from './base.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-export default [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  ...base,
-];
+export default [...coreWebVitals, ...nextTypescript, ...base];
