8ee2fa3b73ac026ff13b455807ab38d64287ba0d
raiton-boo <raito.build@gmail.com>
2026-09-02 17:51:09 +0900

    feat: クイズCRUD APIを実装

---
+++ apps/api/src/quiz/quiz.controller.ts
@@ -0,0 +1,58 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+import type { Request } from 'express';
+
+import { CreateQuizDto } from './dto/create-quiz.dto.js';
+import { UpdateQuizDto } from './dto/update-quiz.dto.js';
+import { QuizService } from './quiz.service.js';
+
+@Controller('quizzes')
+@UseGuards(AuthGuard('jwt'))
+export class QuizController {
+  constructor(private readonly quizService: QuizService) {}
+
+  @Get()
+  getQuizzes(@Req() req: Request) {
+    const authenticatedUser = req.user as { id: string };
+    return this.quizService.getQuizzes(authenticatedUser.id);
+  }
+
+  @Get(':id')
+  getQuizById(@Req() req: Request, @Param('id') id: string) {
+    const authenticatedUser = req.user as { id: string };
+    return this.quizService.getQuizById(authenticatedUser.id, id);
+  }
+
+  @Post()
+  createQuiz(@Req() req: Request, @Body() dto: CreateQuizDto) {
+    const authenticatedUser = req.user as { id: string };
+    return this.quizService.createQuiz(authenticatedUser.id, dto);
+  }
+
+  @Patch(':id')
+  updateQuiz(
+    @Req() req: Request,
+    @Param('id') id: string,
+    @Body() dto: UpdateQuizDto,
+  ) {
+    const authenticatedUser = req.user as { id: string };
+    return this.quizService.updateQuiz(authenticatedUser.id, id, dto);
+  }
+
+  @Delete(':id')
+  deleteQuiz(@Req() req: Request, @Param('id') id: string) {
+    const authenticatedUser = req.user as { id: string };
+    return this.quizService.deleteQuiz(authenticatedUser.id, id);
+  }
+}
